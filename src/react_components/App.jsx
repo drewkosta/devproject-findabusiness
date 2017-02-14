5e1cb99efe010f57a4efcab6ead78824c2193205
Drew Kosta <drewkosta@gmail.com>
2017-02-14 14:05:42 -0500

    add gmaps api calls

--- src/react_components/App.jsx
+++ src/react_components/App.jsx
@@ -1,11 +1,95 @@
 import React from 'react';
-import SearchForm from './SearchForm.jsx'
+import SearchForm from './SearchForm.jsx';
 
 class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      searchData: {}
+    };
+  }
+  
+  
+  handleSubmit (e) {
+    e.preventDefault();
+    this.getLatLong(
+      document.getElementById('zip').value,
+      data => {      
+        this.getPlaces(
+          new google.maps.LatLngBounds(
+            new google.maps.LatLng(data.results[0].geometry.bounds.southwest.lat, data.results[0].geometry.bounds.southwest.lng),
+            new google.maps.LatLng(data.results[0].geometry.bounds.northeast.lat, data.results[0].geometry.bounds.northeast.lng)
+          ),
+          placeId => { 
+            this.getDetails(
+              placeId,
+              data => console.log(data)  
+            )
+          }
+        )
+      }
+    );
+  }
+  
+  getLatLong (zip, callback) {
+    console.log('in getLatLong')
+    $.get(
+      'http://maps.googleapis.com/maps/api/geocode/json?address=' + zip,
+      data => {
+        callback(data);
+      }
+    )
+  }
+  
+  getPlaces (bounds, callback) {
+    console.log('in getPlaces')
+      const mapOptions = {
+        zoom: 1,
+        center: new google.maps.LatLng(0,0)
+      };
+      const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      console.log(map)
+      const request = {
+        bounds: bounds,
+        keyword: document.getElementById('phone').value,
+        name: document.getElementById('name').value
+      };
+      
+      const gMaps = new google.maps.places.PlacesService(map);
+      gMaps.nearbySearch(
+        request,
+        data => {
+          console.log(data);
+          if (data[0]) {
+            callback(data[0].place_id);
+          }
+        }
+      );
+  }
+  
+  getDetails (placeId, callback) {
+    console.log(placeId)
+    const mapOptions = {
+      zoom: 1,
+      center: new google.maps.LatLng(0,0)
+    };
+    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    const gMaps = new google.maps.places.PlacesService(map);
+    gMaps.getDetails(
+      { placeId: placeId },
+      data => {
+        console.log(data)
+        console.log(data.photos[0].getUrl({ 'maxWidth': 500, 'maxHeight': 500 }))
+        callback(data);
+      }
+    );
+  }
+  
   render() {
     return (
       <div>
-        <SearchForm />
+        <SearchForm handleSubmit={this.handleSubmit.bind(this)} />
       </div>
     )
   }
--- src/react_components/SearchForm.jsx
+++ src/react_components/SearchForm.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const SearchForm = props => {
     return (
       <form onSubmit={props.handleSubmit}>
-        <div>Business Name: <input id="number"></input></div>
-        <div>Phone Number: <input id="name"></input></div>
-        <div>Zip Code: <input id="types"></input></div>
+        <div>Business Name: <input id="name"></input></div>
+        <div>Phone Number: <input id="phone"></input></div>
+        <div>Zip Code: <input id="zip"></input></div>
         <button type="submit">submit</button>
       </form>
     )
